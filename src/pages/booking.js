8eb7701b698b4e656f344efb0d14692612f77b6d
thomaswatchman <thomas@watchmans.com>
2026-03-09 13:31:27 -0300

    added sample availabilites for, will need to come from backend eventually

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -1,5 +1,8 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
+const APPOINTMENT_STATE_UNCONFIRMED = "APPOINTMENT_STATE_UNCONFIRMED";
+const APPOINTMENT_LENGTH_MINUTES = 30;
+
 const WEEKDAYS = [
   { label: "Sunday", value: 0 },
   { label: "Monday", value: 1 },
@@ -8,6 +11,16 @@ const WEEKDAYS = [
   { label: "Thursday", value: 4 },
   { label: "Friday", value: 5 },
   { label: "Saturday", value: 6 },
+];
+
+// examples availabilities for now (lenght in minutes)
+const AVAILABILITIES = [
+  { week_day: 1, starting_hour: 9, starting_minute: 0, length: 30 },
+  { week_day: 1, starting_hour: 13, starting_minute: 0, length: 60 },
+  { week_day: 2, starting_hour: 10, starting_minute: 0, length: 120 },
+  { week_day: 3, starting_hour: 11, starting_minute: 0, length: 30 },
+  { week_day: 4, starting_hour: 9, starting_minute: 30, length: 60 },
+  { week_day: 5, starting_hour: 12, starting_minute: 0, length: 45 },
 ];
 
 export function BookingScreen() {
