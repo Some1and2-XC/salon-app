03345298ee58b9f2b58c5eef50de3fe65e35d763
Sairaj Jadhav <sr819897@dal.ca>
2026-04-03 14:49:01 -0300

    Revert "Merge branch 'admin_validate' into 'main'"
    This reverts merge request !18

--- src/pages/qrGenerator.js
+++ src/pages/qrGenerator.js
@@ -1,27 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 
 import { sty } from "../styles";
 
-export function QRScreen({ route }) {
+export function QRScreen({route}) {
 
-    const { userId, appointmentId } = route.params;
-    const [qrValue, setQrValue] = useState('');
+    const qrValue = JSON.stringify(route.params.data);
 
-    useEffect(() => {
-        const qrData = JSON.stringify({
-            userId,
-            appointmentId
-        });
-
-        setQrValue(qrData);
-    }, [userId, appointmentId]);
+    // TODO remove this log call (or console.trace or something...).
+    console.log(qrValue);
 
     return (
-        <View style={sty.container}>
-            <Text>Check-in ready! Show this QR to the admin.</Text>
-
+        <View styles={sty.container}>
+            <Text>Checkin Confirmed! Use this to checkin for your appointment!</Text>
             {qrValue !== '' && (
                 <View style={sty.containerCentered}>
                     <QRCode value={qrValue} size={250} />
