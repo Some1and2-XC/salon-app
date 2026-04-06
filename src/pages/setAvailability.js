ada2f68c87ec39ae5610101c1834d4c3260bbba0
Sairaj Jadhav <sr819897@dal.ca>
2026-04-06 02:16:26 -0300

    Edit setAvailability.js

--- src/pages/setAvailability.js
+++ src/pages/setAvailability.js
@@ -15,7 +15,7 @@ import { apiFetch, assertFetchSuccessful } from "../utils";
 import { colorSchemeGreens } from "../colorScheme";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import { makeStyles as makeBookingStyles, OptionModal } from "../pages/booking";
-import { AdminBackBar } from "../pages/AdminBackBar"; 
+import { AdminBackBar } from "../components/AdminBackBar"; 
 
 function showAlert(title, message) {
     if (Platform.OS === "web") {
