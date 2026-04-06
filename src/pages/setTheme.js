4c84b33182db674c0ed46e7b183ff8202f754f90
Sairaj Jadhav <sairajj8641@gmail.com>
2026-04-06 20:21:50 -0300

    Replaced unicode icons with FontAwesome icons across app

--- src/pages/setTheme.js
+++ src/pages/setTheme.js
@@ -2,6 +2,7 @@ import { useTheme } from "../styles";
 import { MAP_COLOR_SCHEME } from "../colorScheme";
 import { OptionModal } from "../components/OptionModal";
 import { BackButton } from "../components/BackButton";
+import { IconPalette } from "../icons";
 
 import { useState } from "react";
 import { Pressable, ScrollView, Text, View } from "react-native";
@@ -41,9 +42,12 @@ export function SetThemeScreen({ navigation }) {
                 ]}
                 onPress={() => setShowSelectionModal(true) }
             >
-                <Text style={commonUi.auth.inlineCtaButtonText}>
-                    Set Theme &#x1F3A8;
-                </Text>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Text style={commonUi.auth.inlineCtaButtonText}>
+                        Set Theme
+                    </Text>
+                    <IconPalette size={16} style={{ marginLeft: 6 }} />
+                </View>
             </Pressable>
 
             <OptionModal
