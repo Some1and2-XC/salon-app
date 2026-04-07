5879c7248a9c2f82845014ba94773e4cbc025d17
some1and2 <mkpt00@gmail.com>
2026-04-06 22:47:00 -0300

    Removed hard-coded color

--- src/pages/home.js
+++ src/pages/home.js
@@ -229,7 +229,7 @@ export function HomeScreen({ navigation, route }) {
                         <Text style={commonUi.auth.inlineCtaButtonText}>
                             Set Theme
                         </Text>
-                        <IconPalette size={16} color="#fff" style={{ marginLeft: 6 }} />
+                        <IconPalette size={16} style={{ marginLeft: 6 }} />
                     </View>
                 </Pressable>
 
